d3aea0ca9855e24e6519e28decd56ed8159976ac
Guitoon <vergeot.gaetan@gmail.com>
2019-12-15 16:36:01 +0100

    Le rocher system

--- Assets/_Core/Gameplay/Elements/Rock/Rock.cs
+++ Assets/_Core/Gameplay/Elements/Rock/Rock.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Rock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEvent OnRockSlashed;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        OnRockSlashed.Invoke();
     }
 }
