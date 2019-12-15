022b7c8c9fdd795bd6711072f11c1df99ec011f8
Julien <julien.delaunay034@gmail.com>
2019-12-15 17:52:06 +0100

    Added slash VFX

--- Assets/_Core/Gameplay/Elements/Tree/SlashableTreeEffects.cs
+++ Assets/_Core/Gameplay/Elements/Tree/SlashableTreeEffects.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class SlashableTreeEffects : MonoBehaviour
 {
     public Animator _anim;
+    public ParticleSystem _slashEffect = null;
 
     public void OnSlash() {
         //_anim.SetTrigger("Slash");
+        Instantiate(_slashEffect, transform.position, Quaternion.identity);
     }
 }
