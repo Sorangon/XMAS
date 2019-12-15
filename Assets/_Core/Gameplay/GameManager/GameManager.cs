9e0b83bfbd25ac35c52c5a998294d6435abbd99d
Julien <julien.delaunay034@gmail.com>
2019-12-15 15:36:44 +0100

    Added a log counter

--- Assets/_Core/Gameplay/GameManager/GameManager.cs
+++ Assets/_Core/Gameplay/GameManager/GameManager.cs
@@ -4,4 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public Spawner spawner = null;
+	private int _logs = 0;
+	public int Logs => _logs;
+
+	private void Start() {
+		StartGame();
+	}
+
+	public void StartGame() {
+		spawner.Spawn(6, 0);
+	}
+
+	public void CollectLog(int collected) {
+		_logs += collected;
+	}
 }
