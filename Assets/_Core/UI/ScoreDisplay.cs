0717136ff83131de2b58021aa5c79aa508231b4c
Guitoon <vergeot.gaetan@gmail.com>
2019-12-15 17:04:19 +0100

    Le score merde

---
+++ Assets/_Core/UI/ScoreDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public PlayerScoreData playerScoreData;
+    public TextMeshProUGUI scoreText;
+
+    void Update()
+    {
+        scoreText.text = playerScoreData.playerScore.ToString();
+    }
+}
