d43d5e72cb36a267e62508fd266bf81ed369740f
Julien <julien.delaunay034@gmail.com>
2019-12-15 16:43:39 +0100

    Anim trees

---
+++ Assets/_Core/Gameplay/Elements/Tree/SlashableTreeEffects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashableTreeEffects : MonoBehaviour
+{
+    public Animator _anim;
+
+    public void OnSlash() {
+        //_anim.SetTrigger("Slash");
+    }
+}
